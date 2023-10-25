d78229eede8b4e0fe50ac023e017d818715171f8
Daniel juliao <zigmainflables@gmail.com>
2023-10-25 00:28:33 -0500

    1.0.2

--- src/utils/PlantillaResponse.ts
+++ src/utils/PlantillaResponse.ts
@@ -2,27 +2,9 @@
 import { HttpStatus } from '@nestjs/common';
 
 export class PlantillaResponse<E> {
-  isRta: boolean;
-  message: string;
-  httpStatus: HttpStatus;
-  data: E;
+  isRta?: boolean;
+  message?: string;
+  httpStatus?: HttpStatus;
+  data?: E;
   dataList?: E[];
-
-
-  constructor(
-    isRta: boolean,
-    message: string,
-    httpStatus: HttpStatus,
-    data: E,
-    dataList?: E[],
-
-  ) {
-    this.isRta = isRta;
-    this.message = message;
-    this.httpStatus = httpStatus;
-    this.data = data;
-    this.dataList = dataList;
-
-  }
 }
-
